af0f476411d7b1246c06768a9b6365fbd4e571c2
Simeon Widdis <sawiddis@gmail.com>
2020-11-13 11:04:15 -0700

    Implement room API GET endpoint stub

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const pug = require('pug')
 const path = require('path');
-const rendering = require('./routes/render');
+const render = require('./routes/render');
+const rooms = require('./routes/rooms');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', routes.index);
+app.get('/', render.index);
+app.get('/room/:room_id', rooms.getRoom);
 
 app.listen(3000);
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,6 +1,18 @@
+/**
+ * Room API endpoints
+ * RESTful API for room CRUD operations
+ * 
+ * API is accessed at the /room path
+ */
+
 exports.createRoom = () => {};
 
-exports.getRoom = () => {};
+// Room GET API endpoint
+// Takes room ID as path variable, e.g. /room/public
+exports.getRoom = (req, res) => {
+    let room_id = req.params.room_id;
+    res.json({room_id});
+};
 
 exports.sendToRoom = () => {};
 
