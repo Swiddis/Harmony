63253deaf3b74c532b306a6ca13356817de46267
Simeon Widdis <sawiddis@gmail.com>
2020-11-16 10:09:48 -0700

    Expand room API method stubs

--- routes/rooms.js
+++ routes/rooms.js
@@ -5,17 +5,87 @@
  * API is accessed at the /room path
  */
 
-exports.createRoom = () => {};
+ const db = require('../db/roomdb');
+
+ // Room POST API endpoint
+ // Post to /room, accepts input in JSON format
+exports.createRoom = (req, res) => {
+    let room = {
+        room_id: req.body.id,
+        room_title: req.body.title,
+        password: req.body.password,
+        nicknames: req.body.nicknames
+    };
+
+    try {
+        db.createRoom(room);
+        res.status(204);
+        res.send();
+    } catch (err) {
+        response = {
+            'timestamp': new Date().toISOString(),
+            'status': 500,
+            'error': err.message(),
+            'path': '/room'
+        };
+        res.status(500);
+        res.json(response);
+    }
+};
 
 // Room GET API endpoint
 // Takes room ID as path variable, e.g. /room/public
 exports.getRoom = (req, res) => {
     let room_id = req.params.room_id;
-    res.json({room_id});
+    let response;
+
+    try {
+        let room = db.getRoom(room_id);
+        response = {
+            'timestamp': new Date().toISOString(),
+            'status': 200,
+            'data': room,
+            'path': '/room/' + encodeURIComponent(room.room_id)
+        };
+    } catch (err) {
+        response = {
+            'timestamp': new Date().toISOString(),
+            'status': 500,
+            'error': err.message(),
+            'path': '/room'
+        };
+        res.status(500);
+    }
+
+    res.json(response);
 };
 
-exports.sendToRoom = () => {};
+// Room messaging POST endpoint
+// Post to /room/room_id
+exports.sendToRoom = (req, res) => {
+    let room_id = req.params.room_id;
+    let message = req.body.message;
+    
+    try {
+        db.sendToRoom(room_id, message);
+        res.status(204);
+        res.send();
+    } catch (err) {
+        response = {
+            'timestamp': new Date().toISOString(),
+            'status': 500,
+            'error': err.message(),
+            'path': '/room'
+        };
+        res.status(500);
+        res.json(response);
+    }
+};
 
-exports.authorizeRoomAccess = () => {};
+exports.authorizeRoomAccess = (req, res) => {
+    // TODO
+};
 
-exports.establishUserDMs = () => {};
+exports.establishUserDMs = (req, res) => {
+    // TODO
+};
