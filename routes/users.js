3ef0d9034a2bc4454d16d458847fc3bcb002e161
Simeon Widdis <sawiddis@gmail.com>
2020-11-17 10:44:41 -0700

    Flesh out user authentication endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,7 @@
 // User POST endpoint
+
+const { response } = require("express");
+
 // Post to /user, accepts input in JSON format
 exports.createUser = (req, res) => {
     let user = {
@@ -42,7 +45,11 @@ exports.deleteUser = (req, res) => {
 // User authentication endpoint
 // Authenticate /user/authenticate
 exports.authenticateUser = (req, res) => {
-    // TODO, utilize database
+    let username = req.body.username;
+    let password = req.body.password;
+
+    db.authenticateUser(username, password, buildAuthResponse);
+
     res.json({
         'timestamp': new Date().toISOString(),
         'status': 200,
@@ -63,7 +70,7 @@ const buildResponse = (err, user) => {
         }
         if (err.message() == "User not found") {
             response.status = 404;
-        } else if (err.message == "User already exists") {
+        } else if (err.message() == "User already exists") {
             response.status = 403;
         }else {
             response.status = 500;
@@ -80,3 +87,32 @@ const buildResponse = (err, user) => {
     res.status(response.status);
     res.json(response);
 };
+
+const buildAuthResponse = (err, authorities) => {
+    let response;
+
+    if (err) {
+        response = {
+            'timestamp': new Date().toISOString(),
+            'path': '/user/authenticate',
+            'error': err.message()
+        }
+        if (err.message() == "User not found") {
+            response.status = 404;
+        } else if (err.message() == "Invalid credentials") {
+            response.status = 401;
+        } else {
+            response.status = 500;
+        }
+    } else {
+        response = {
+            'timestamp': new Date().toISOString(),
+            'status': 200,
+            'path': '/user/authenticate',
+            'authorities': authorities
+        }
+    }
+
+    res.status(response.status);
+    res.json(response);
+}
