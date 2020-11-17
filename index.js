203b2e8438758e6080f575cd7444c3c097cedbf0
Travja <the.only.t.craft@gmail.com>
2020-11-17 10:26:21 -0700

    Begin stubs for socket.io

--- index.js
+++ index.js
@@ -8,6 +8,26 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+    console.log("Received connection. Waiting for credentials.");
+
+    socket.on('username', (username) => {
+        console.log(username + " connected");
+    });
+
+    socket.on('disconnect', (username) => {
+        console.log("Received disconnect request for user " + username);
+    });
+
+    socket.on('message', (message) => {
+        console.log("Received message: ");
+        console.log(message);
+    });
+});
+
+
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, '/public')));
@@ -30,4 +50,10 @@ app.get('/user/:usermame', users.getUser);
 app.patch('/user/:username', urlencodedParser, users.updateUser);
 app.get('/user/authenticate', users.authenticateUser);
 
-app.listen(3000);
+app.get('/testsocket', (req, res) => {
+    res.render('testsocket')
+});
+
+server.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
---
+++ public/scripts/socketmanager.js
@@ -0,0 +1,2 @@
+const socket = io.connect(document.location.host);
+console.log(document.location);
