22e45bd4452e043bb5f6decab434f10ec9ce30ee
Simeon Widdis <sawiddis@gmail.com>
2020-11-20 10:38:51 -0700

    Add missing semicolons in index

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 const urlencodedParser = bodyParser.urlencoded({
     extended: true
-})
+});
 
 const checkAuth = (req, res, next) => {
     if (req.session.user && req.session.user.isAuthenticated) {
@@ -37,12 +37,12 @@ app.use(
         saveUninitialized: true,
         resave: true
     })
-)
+);
 
 app.get('/', render.index);
 
 // app.get("/room", render.rooms)
-app.get("/room", checkAuth, render.rooms)
+app.get("/room", checkAuth, render.rooms);
 
 app.get("/login", render.login);
 app.post("/login", urlencodedParser, render.checkAccess);
@@ -65,10 +65,10 @@ app.post("/signup", urlencodedParser, users.createUser); // Redundant?
 app.get('/testsocket', (req, res) => {
     res.render('testsocket', {
         username: (req.session.user ? req.session.user.username : undefined)
-    })
+    });
 });
 
-app.get("/logout", render.logout)
+app.get("/logout", render.logout);
 
 server.listen(3000, () => {
     console.log("Listening on port 3000");
