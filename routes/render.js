9bb3a9f5a8ef4259d1f3bcdb358961893fb48606
Travja <the.only.t.craft@gmail.com>
2020-12-01 18:39:59 -0700

    Give users feedback of invalid credentials when failing to log in

--- routes/render.js
+++ routes/render.js
@@ -1,98 +1,100 @@
 const config = require("../config.json");
-const { User } = require("../conf/mongo_conf");
+const {User} = require("../conf/mongo_conf");
 const userdb = require("../db/userdb");
 
 var allowed;
 
 exports.index = (req, res) => {
-  res.render("index", {
-    title: "Home",
-    config: config,
-  });
+    res.render("index", {
+        title: "Home",
+        config: config,
+    });
 };
 
 exports.login = (req, res) => {
-  res.render("login", {
-    title: "Login",
-    config: config,
-  });
+    let dat = {
+        title: "Login",
+        config: config,
+        invalid: (Object.keys(req.query).length > 0 && req.query.invalid.length >= 0)
+    };
+    res.render("login", dat);
 };
 
 exports.signUp = (req, res) => {
-  res.render("create", {
-    title: "Sign Up",
-    config: config,
-  });
+    res.render("create", {
+        title: "Sign Up",
+        config: config,
+    });
 };
 
 exports.rooms = (req, res) => {
-  if (!req.session.user) {
-    res.redirect("/login");
-    return;
-  }
-  User.findOne(
-    {
-      username: req.session.user.username,
-    },
-    (err, user) => {
-      if (err) {
-        res.json(err);
-        return;
-      }
-      if (!user) {
+    if (!req.session.user) {
         res.redirect("/login");
         return;
-      }
-      res.render("room", {
-        title: "Room",
-        users: user,
-        //The user should exist and be passed into the render
-        // Since this should be an authenticated end-point, this works.
-        // TestUser1's password is testing123
-        // Other accounts should be able to be created using the sign up page.
-        username: user.username,
-        avatar: user.avatar,
-      });
     }
-  );
+    User.findOne(
+        {
+            username: req.session.user.username,
+        },
+        (err, user) => {
+            if (err) {
+                res.json(err);
+                return;
+            }
+            if (!user) {
+                res.redirect("/login");
+                return;
+            }
+            res.render("room", {
+                title: "Room",
+                users: user,
+                //The user should exist and be passed into the render
+                // Since this should be an authenticated end-point, this works.
+                // TestUser1's password is testing123
+                // Other accounts should be able to be created using the sign up page.
+                username: user.username,
+                avatar: user.avatar,
+            });
+        }
+    );
 };
 
 exports.checkAccess = (req, res) => {
-  if (req.body.username == "" || req.body.password == null) {
-    res.redirect("/login");
-  }
-
-  let userName = req.body.username;
-  let userPassword = req.body.password;
-  console.log("Attempting to authenticate user " + userName);
-
-  userdb.authenticateUser(userName, userPassword, (err, user) => {
-    if (err) {
-      if (
-        err.message == "Invalid credentials" ||
-        err.message == "User not found"
-      ) {
-        res.redirect("/login");
-        return;
-      }
+    if (req.body.username == "" || req.body.password == null) {
+        res.redirect("/login?invalid");
     }
 
-    console.log("User authenticated!");
-    req.session.user = {
-      isAuthenticated: true,
-      username: req.body.username,
-    };
-    allowed = userName;
-    res.redirect("/room");
-  });
+    let userName = req.body.username;
+    let userPassword = req.body.password;
+    console.log("Attempting to authenticate user " + userName);
+
+    userdb.authenticateUser(userName, userPassword, (err, user) => {
+        if (err) {
+            if (
+                err.message == "Invalid credentials" ||
+                err.message == "User not found"
+            ) {
+                res.redirect("/login?invalid");
+                return;
+            }
+        }
+
+        console.log("User authenticated!");
+        req.session.user = {
+            isAuthenticated: true,
+            username: req.body.username,
+        };
+        allowed = userName;
+        res.redirect("/room");
+    });
 };
 
 exports.logout = (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.redirect("/");
-    }
-  });
+    req.session.destroy((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    });
 };
