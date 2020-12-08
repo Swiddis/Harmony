99fb2d082ecf0241724bc71b54455979c9b65fdc
Travja <the.only.t.craft@gmail.com>
2020-12-08 10:24:10 -0700

    Don't use srv

--- conf/mongo_conf.js
+++ conf/mongo_conf.js
@@ -25,7 +25,7 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 mongoose.connect(
-    "mongodb+srv://admin:Ydp4ZCmttqp2zPj@harmonyclient.xyz/data?authSource=admin&retryWrites=true&w=majority",
+    "mongodb://admin:Ydp4ZCmttqp2zPj@harmonyclient.xyz/data?authSource=admin&retryWrites=true&w=majority",
     {
         useUnifiedTopology: true,
         useNewUrlParser: true,
